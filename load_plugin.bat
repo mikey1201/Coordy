fe68fd55382f4d20c617622a5de762f65cf83a30
Michael Wegiel <159095682+mikey1201@users.noreply.github.com>
2024-08-18 11:33:36 -0400

    Delete load_plugin.bat

--- load_plugin.bat
+++
@@ -1,3 +0,0 @@
-del "C:\Users\Michael\intellij\projects\Coordy\Plugin Test Server\plugins\Coordy-1.6.01.jar"
-copy "C:\Users\Michael\intellij\projects\Coordy\target\Coordy-1.6.01.jar" "C:\Users\Michael\intellij\projects\Coordy\Plugin Test Server\plugins"
-rd /s /q "C:\Users\Michael\intellij\projects\Coordy\Plugin Test Server\plugins\.paper-remapped"
